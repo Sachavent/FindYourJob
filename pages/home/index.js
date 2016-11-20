26d779f746141210e47eb41a3b9145a13eb9a28d
Sachavent <ventribout@et.esiea.fr>
2016-11-20 23:57:55 +0100

    First selectfield
    watch later for ontouchtap

--- pages/home/index.js
+++ pages/home/index.js
@@ -4,18 +4,24 @@ import s from './styles.css';
 import { title, html } from './index.md';
 
 class HomePage extends React.Component {
-
-  static propTypes = {
-    articles: PropTypes.array.isRequired,
-  };
-
-  componentDidMount() {
-    document.title = title;
+  constructor(props) {
+    super(props);
+    this.state = {value: "A"};
   }
 
+  handleChange = (event, index, value) => this.setState({value});
   render() {
-    return (
+
+
+      return (
       <Layout className={s.content}>
+        <div>
+          <select value={this.state.value} onChange={this.handleChange}>
+            <option value="A">Apple</option>
+            <option value="B">Banana</option>
+            <option value="C">Cranberry</option>
+          </select>
+          </div>
 
       </Layout>
     );
