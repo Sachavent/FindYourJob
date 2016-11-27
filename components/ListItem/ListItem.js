b116d8cf4f598608bc52ff248c7e0982286fb4e3
Sachavent <ventribout@et.esiea.fr>
2016-11-28 00:19:44 +0100

    search update

--- components/ListItem/ListItem.js
+++ components/ListItem/ListItem.js
@@ -3,15 +3,28 @@
  */
 import React from 'react';
 
-
 class ListItem extends React.Component {
-
+  constructor(props) {
+    super(props);
+  }
   render() {
+    function readthis(dataRecieve, i ) {
+      if (dataRecieve.zipcode == zipcode) {
+        return <p key={'recherche index-' + i}>{dataRecieve.contact_email}</p>
+      }
+    }
+
+    var zipcode;
     return (
-      <p>{this.props.value}</p>
+      <div>
+        <p>{this.props.value}</p>
+        {
+          this.props.dataRecieve.map(readthis, zipcode = this.props.value)}
+      </div>
     );
   }
 
 }
+
 
 export default ListItem;
--- pages/home/index.js
+++ pages/home/index.js
@@ -1,7 +1,7 @@
-import React, { PropTypes } from 'react';
+import React, {PropTypes} from 'react';
 import Layout from '../../components/Layout';
 import s from './styles.css';
-import { title, html } from './index.md';
+import {title, html} from './index.md';
 import FlatButton from 'material-ui/FlatButton';
 import SearchIcon from 'material-ui/svg-icons/action/search';
 import SelectField from 'material-ui/SelectField';
@@ -13,7 +13,8 @@ class HomePage extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = {value: 75001,
+    this.state = {
+      value: 75001,
       dataRecieve: [],
     };
   }
@@ -23,57 +24,50 @@ class HomePage extends React.Component {
   /*Getting Json Data before rendering the page */
   componentWillMount() {
     var data = require('./data.json');
-    this.setState( { dataRecieve:data});
+    this.setState({dataRecieve: data});
   }
 
   render() {
     return (
       <Layout className={s.content}>
-        <div className="mdl-grid" style={{backgroundColor:'grey'}}>
+        <div className="mdl-grid" style={{backgroundColor: 'grey'}}>
           <div className="mdl-cell mdl-cell--3-col">
             <SelectField value={this.state.value} onChange={this.handleChange}>
-              <MenuItem value={75001} primaryText="Paris, 75001" />
-              <MenuItem value={75002} primaryText="Paris, 75002" />
-              <MenuItem value={75003} primaryText="Paris, 75003" />
-              <MenuItem value={75004} primaryText="Paris, 75004" />
-              <MenuItem value={75005} primaryText="Paris, 75005" />
-              <MenuItem value={75006} primaryText="Paris, 75006" />
-              <MenuItem value={75007} primaryText="Paris, 75007" />
-              <MenuItem value={75008} primaryText="Paris, 75008" />
-              <MenuItem value={75009} primaryText="Paris, 75009" />
-              <MenuItem value={75010} primaryText="Paris, 75010" />
-              <MenuItem value={75011} primaryText="Paris, 75011" />
-              <MenuItem value={75012} primaryText="Paris, 75012" />
-              <MenuItem value={75013} primaryText="Paris, 75013" />
-              <MenuItem value={75014} primaryText="Paris, 75014" />
-              <MenuItem value={75015} primaryText="Paris, 75015" />
-              <MenuItem value={75016} primaryText="Paris, 75016" />
-              <MenuItem value={75017} primaryText="Paris, 75017" />
-              <MenuItem value={75018} primaryText="Paris, 75018" />
-              <MenuItem value={75019} primaryText="Paris, 75019" />
-              <MenuItem value={75020} primaryText="Paris, 75020" />
+              <MenuItem value={75001} primaryText="Paris, 75001"/>
+              <MenuItem value={75002} primaryText="Paris, 75002"/>
+              <MenuItem value={75003} primaryText="Paris, 75003"/>
+              <MenuItem value={75004} primaryText="Paris, 75004"/>
+              <MenuItem value={75005} primaryText="Paris, 75005"/>
+              <MenuItem value={75006} primaryText="Paris, 75006"/>
+              <MenuItem value={75007} primaryText="Paris, 75007"/>
+              <MenuItem value={75008} primaryText="Paris, 75008"/>
+              <MenuItem value={75009} primaryText="Paris, 75009"/>
+              <MenuItem value={75010} primaryText="Paris, 75010"/>
+              <MenuItem value={75011} primaryText="Paris, 75011"/>
+              <MenuItem value={75012} primaryText="Paris, 75012"/>
+              <MenuItem value={75013} primaryText="Paris, 75013"/>
+              <MenuItem value={75014} primaryText="Paris, 75014"/>
+              <MenuItem value={75015} primaryText="Paris, 75015"/>
+              <MenuItem value={75016} primaryText="Paris, 75016"/>
+              <MenuItem value={75017} primaryText="Paris, 75017"/>
+              <MenuItem value={75018} primaryText="Paris, 75018"/>
+              <MenuItem value={75019} primaryText="Paris, 75019"/>
+              <MenuItem value={75020} primaryText="Paris, 75020"/>
             </SelectField>
           </div>
           <div className="mdl-cell mdl-cell--2-col">
-          <FlatButton
-            icon={<SearchIcon />}
-            onClick={()=> {
-              console.log("statefield values recieve: "+this.state.value);
-            }}
-          />
+            <FlatButton
+              icon={<SearchIcon />}
+              onClick={()=> {
+                console.log("statefield values recieve: " + this.state.value);
+              }}
+            />
           </div>
         </div>
 
         <div className="demo-grid-2 mdl-grid">
           <div className="mdl-cell mdl-cell--4-col">
-             liste items
             <ListItem {...this.state}/>
-            {
-              this.state.dataRecieve.map(function(dataRecieve, i) {
-                if (dataRecieve.zipcode== "75001") {
-                  return <p key={'recherche index-' + i}>{dataRecieve.contact_email}</p>
-                }
-              })}
           </div>
           <div className="mdl-cell mdl-cell--8-col">
             <GoogleMap/>
